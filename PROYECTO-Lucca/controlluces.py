b0d5ec5ad497b6f23f33c837c04048095e52801a
lukeshawww1 <159242780+lukeshawww1@users.noreply.github.com>
2025-05-15 18:42:02 +0200

    Comentarios script

--- PROYECTO-Lucca/controlluces.py
+++ PROYECTO-Lucca/controlluces.py
@@ -1,23 +1,23 @@
-import paho.mqtt.client as mqtt  # Importa la librería MQTT para Python
+iimport paho.mqtt.client as mqtt  # Importa la librería MQTT para Python
 
 # Función que se ejecuta al conectar con el broker MQTT
 def on_connect(client, userdata, flags, rc):
     print("[INFO] Conectado con código:", rc)  # Código de resultado de la conexión
-    client.subscribe("casa/luz")  # Se suscribe al tópico 'casa/luz'
+    client.subscribe("casa/luz")  # Se suscribe al tópico 'casa/luz' para publicarlo a la web después
     print("[INFO] Suscrito al topic 'casa/luz'")
 
 # Función que se ejecuta cuando se recibe un mensaje en un tópico suscrito
 def on_message(client, userdata, msg):
-    comando = msg.payload.decode()  # Decodifica el mensaje recibido
+    comando = msg.payload.decode()  # Decodifica el mensaje recibido para que pueda ser leído e interpretado
     print("[INFO] Mensaje recibido:", comando)
 
-    # Verifica el comando y publica la orden correspondiente
+    # Verifica el comando y publica la orden que le doy. El tópico lo he cambiado ya que también se lo he cambiado a la bombilla.
     if comando == "ON":
         print("[INFO] Enviando comando de encendido a bombilla...")
-        client.publish("shellies/shellycolorbulb-FCF5C4B2D978/color/0/command", "on")
+        client.publish("shellies/shellies/color/0/command", "on")
     elif comando == "OFF":
         print("[INFO] Enviando comando de apagado a bombilla...")
-        client.publish("shellies/shellycolorbulb-FCF5C4B2D978/color/0/command", "off")
+        client.publish("shellies/shellies/color/0/command", "off")
     else:
         print("[ERROR] Comando desconocido:", comando)
 
@@ -28,7 +28,7 @@ mqtt_client = mqtt.Client()
 mqtt_client.on_connect = on_connect
 mqtt_client.on_message = on_message
 
-# Conecta al broker MQTT en localhost por el puerto 1883 con un keepalive de 60 segundos
+# Conecta al broker MQTT en localhost por el puerto 1883 con un keepalive de 60 segundos para mantenerlo activo
 mqtt_client.connect("localhost", 1883, 60)
 
 # Mantiene el cliente en ejecución para seguir recibiendo mensajes
